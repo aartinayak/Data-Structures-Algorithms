8e117aac05201906084595054328f7cd148f3905
Aarti Nayak <aartinayak2610@gmail.com>
2023-06-01 13:23:46 -0400

    Added files for LL cycle detection

---
+++ Linked-List/LinkedListCycle.py
@@ -0,0 +1,64 @@
+"""
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. 
+Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+Return true if there is a cycle in the linked list. Otherwise, return false.
+"""
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+        
+        
+def has_cycle(head) -> bool:
+    
+    slow, fast = head, head
+
+    while fast and fast.next:
+
+        fast = fast.next.next
+        slow = slow.next
+
+        if slow == fast:
+            return True
+
+
+    return False
+
+def main():
+    
+    """ 
+    CASE 1 
+    1 --> 2 --> 3 --> 4 --> 5 --> 6
+    """
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    head.next.next.next.next = ListNode(5)
+    head.next.next.next.next.next = ListNode(6)
+    print("Linked List has cycle: " + str(has_cycle(head)))
+    
+    
+    """CASE 2
+    Linking the last node to the second 
+    1 --> 2 --> 3 --> 4 --> 5 --> 6
+                ^-----------------^
+    """
+    head.next.next.next.next.next = head.next.next
+    print("Linked List has cycle: " + str(has_cycle(head)))
+    
+    
+    """CASE 2
+    Linking the last node to the third 
+    1 --> 2 --> 3 --> 4 --> 5 --> 6
+                      ^------------^
+    """
+    
+    head.next.next.next.next.next = head.next.next.next
+    print("Linked List has cycle: " + str(has_cycle(head)))
+    
+main()
+    
